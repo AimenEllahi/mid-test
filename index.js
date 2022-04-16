8424ea74c357110b179f7d4ecdbcd9c8fefbcbde
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2022-04-16 18:30:31 +0500

    Corrected Update Functionality
    Well i just corrected the way you were importing those cdns and used preventDeafault in update

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-$(document).ready(function(){
-    //calling functions here
-      loadRecipes();
-      $(".product-section").on("click", ".delete-btn", handleDelete);
-      $(".product-section").on("click", ".edit-btn", handleUpdate);
-      $("#add").click(addRecipe);
-      $("#save-changes").click(saveRecipes);
-    });
+$(document).ready(function () {
+  //calling functions here
+  loadRecipes();
+  $(".product-section").on("click", ".delete-btn", handleDelete);
+  $(".product-section").on("click", ".edit-btn", handleUpdate);
+  $("#add").click(addRecipe);
+  $("#save-changes").click(saveRecipes);
+});
 
 //function to load recipes
-const loadRecipes = function() {
-    $.ajax({
-        url:  "https://usman-recipes.herokuapp.com/api/recipes",
-        method: "GET",
-        error: function(response) {
-           var div = $(".product-section");
-         div.html("An Error has occured");
-        },
-        success: function(response) {
-          console.log(response);
-          var div = $(".product-section");
-              div.empty();
-              for(var i =0; i < response.length; i++){
-                  div.append(
-                    `<div class="col-lg-3 col-md-5 col-10 productCard-1 mt-5" data-id="${response[i]._id}">
+const loadRecipes = function () {
+  $.ajax({
+    url: "https://usman-recipes.herokuapp.com/api/recipes",
+    method: "GET",
+    error: function (response) {
+      var div = $(".product-section");
+      div.html("An Error has occured");
+    },
+    success: function (response) {
+      console.log(response);
+      var div = $(".product-section");
+      div.empty();
+      for (var i = 0; i < response.length; i++) {
+        div.append(
+          `<div class="col-lg-3 col-md-5 col-10 productCard-1 mt-5" data-id="${response[i]._id}">
                     <img class = "product-img" src ="./images/food.jpg">
                      <h2 class="title" >${response[i].title}</h2>
                     <span class="body">${response[i].body}</span>
@@ -30,78 +30,77 @@ const loadRecipes = function() {
                     <button class="own-btn delete-btn">Delete</button>
                 
             </div>`
-                  )
-              } 
-        }
-      });
-  }
-
-
-//function to delete recipe  
-function handleDelete() {
-    var btn = $(this);
-    var parentDiv = btn.closest(".productCard-1");
-    let id = parentDiv.attr("data-id");
-    console.log(id);
-    $.ajax({
-      url: "https://usman-recipes.herokuapp.com/api/recipes/" + id,
-      method: "DELETE",
-      success: function() {
-        loadRecipes();
+        );
       }
-    });
-}
+    },
+  });
+};
 
+//function to delete recipe
+function handleDelete() {
+  var btn = $(this);
+  var parentDiv = btn.closest(".productCard-1");
+  let id = parentDiv.attr("data-id");
+  console.log(id);
+  $.ajax({
+    url: "https://usman-recipes.herokuapp.com/api/recipes/" + id,
+    method: "DELETE",
+    success: function () {
+      loadRecipes();
+    },
+  });
+}
 
 //create recipe function
 function addRecipe() {
-    var title = $(".inputTitle").val();
-    var body = $(".inputBody").val();
+  var title = $(".inputTitle").val();
+  var body = $(".inputBody").val();
 
-    console.log(title,body)
-    $.ajax({
-      url: "https://usman-recipes.herokuapp.com/api/recipes",
-      method: "POST",
-      data: { title,body},
-      success: function(response) {
-        $(".inputTtitle").val("");
-        $(".inputBody").val("");
-        loadRecipes();
-        $("#create").modal("hide");
-      }
-    });
-  } 
+  console.log(title, body);
+  $.ajax({
+    url: "https://usman-recipes.herokuapp.com/api/recipes",
+    method: "POST",
+    data: { title, body },
+    success: function (response) {
+      $(".inputTtitle").val("");
+      $(".inputBody").val("");
+      loadRecipes();
+      $("#create").modal("hide");
+    },
+  });
+}
 
-//handle update  
+//handle update
 function handleUpdate() {
-    var btn = $(this);
-    var parentDiv = btn.closest(".productCard-1");
-    let id = parentDiv.attr("data-id");
-    $.get("https://usman-recipes.herokuapp.com/api/recipes/" + id, function(
-      response
-    ) {
+  var btn = $(this);
+  var parentDiv = btn.closest(".productCard-1");
+  let id = parentDiv.attr("data-id");
+  $.get(
+    "https://usman-recipes.herokuapp.com/api/recipes/" + id,
+    function (response) {
       console.log(response);
       $(".UpdateId").val(response._id);
-      $(".UpdateTitle").val(response.title);  
+      $(".UpdateTitle").val(response.title);
       $(".UpdateBody").val(response.body);
-    });
-  }
+    }
+  );
+}
 
-  function saveRecipes() {
-     //save changes
-        var id = $(".UpdateId").val();
-        var title = $(".UpdateTitle").val();
-        var body = $(".UpdateBody").val();
-        
-        $.ajax({
-         url: "https://usman-recipes.herokuapp.com/api/recipes/" + id,
-         data: { title, body },
-         method: "PUT",
-         success: function(response) {
-           console.log(response);
-           loadRecipes();
-           $("#edit-form").modal("hide");
-         }
-       });
-    
-  }
+function saveRecipes(evt) {
+  //save changes
+  var id = $(".UpdateId").val();
+  var title = $(".UpdateTitle").val();
+  var body = $(".UpdateBody").val();
+  console.log(id, title, body);
+  evt.preventDefault();
+  $.ajax({
+    url: "https://usman-recipes.herokuapp.com/api/recipes/" + id,
+    data: { title, body },
+    method: "PUT",
+    success: function (response) {
+      console.log(response);
+      loadRecipes();
+      $(".updateModal").modal("hide");
+    },
+  });
+}
